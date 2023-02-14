283848c456f3e17076b14634cbe91692fdc96643
Gorgui24 <54688279+Gorgui24@users.noreply.github.com>
2023-02-14 05:28:16 +0100

    Add files via upload

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,56 +1,56 @@
 #!/usr/bin/python3
-""" Unit test Place """
+""" Unit test Review """
 import unittest
 import models
 import os
-from models.place import Place
+from models.review import Review
 
 
-class TestPlace(unittest.TestCase):
-    """ Test for class Place"""
+class TestReview(unittest.TestCase):
+    """ Test for class Review"""
 
     def test_docstring(self):
         '''test if funcions, methods, classes
         and modules have docstring'''
         msj = "Módulo does not has docstring"
-        self.assertIsNotNone(models.place.__doc__, msj)  # Modules
+        self.assertIsNotNone(models.review.__doc__, msj)  # Modules
         msj = "Clase does not has docstring"
-        self.assertIsNotNone(Place.__doc__, msj)  # Classes
+        self.assertIsNotNone(Review.__doc__, msj)  # Classes
 
     def test_executable_file(self):
         '''test if file has permissions u+x to execute'''
         # Check for read access
-        is_read_true = os.access('models/place.py', os.R_OK)
+        is_read_true = os.access('models/review.py', os.R_OK)
         self.assertTrue(is_read_true)
         # Check for write access
-        is_write_true = os.access('models/place.py', os.W_OK)
+        is_write_true = os.access('models/review.py', os.W_OK)
         self.assertTrue(is_write_true)
         # Check for execution access
-        is_exec_true = os.access('models/place.py', os.X_OK)
+        is_exec_true = os.access('models/review.py', os.X_OK)
         self.assertTrue(is_exec_true)
 
-    def test_init_Place(self):
-        """test if an object is an type Place"""
-        my_object = Place()
-        self.assertIsInstance(my_object, Place)
+    def test_init_Review(self):
+        """test if an object is an type Review"""
+        my_object = Review()
+        self.assertIsInstance(my_object, Review)
 
     def test_id(self):
         """ test that id is unique """
-        my_objectId = Place()
-        my_objectId1 = Place()
+        my_objectId = Review()
+        my_objectId1 = Review()
         self.assertNotEqual(my_objectId.id, my_objectId1.id)
 
     def test_str(self):
         '''check if the output of str is in the specified format'''
-        my_strobject = Place()
+        my_strobject = Review()
         _dict = my_strobject.__dict__
-        string1 = "[Place] ({}) {}".format(my_strobject.id, _dict)
+        string1 = "[Review] ({}) {}".format(my_strobject.id, _dict)
         string2 = str(my_strobject)
         self.assertEqual(string1, string2)
 
     def test_save(self):
         """ check if date update when save """
-        my_objectupd = Place()
+        my_objectupd = Review()
         first_updated = my_objectupd.updated_at
         my_objectupd.save()
         second_updated = my_objectupd.updated_at
@@ -60,12 +60,12 @@ class TestPlace(unittest.TestCase):
         '''check if to_dict returns a dictionary, if add a class
         key with class name of the object and if updated_at and
         created_at are converted to string object in ISO format.'''
-        my_model3 = Place()
+        my_model3 = Review()
         my_dict_model3 = my_model3.to_dict()
         self.assertIsInstance(my_dict_model3, dict)
         for key, value in my_dict_model3.items():
             flag = 0
-            if my_dict_model3['__class__'] == 'Place':
+            if my_dict_model3['__class__'] == 'Review':
                 flag += 1
             self.assertTrue(flag == 1)
         for key, value in my_dict_model3.items():
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,46 +1,71 @@
+#!/usr/bin/python3
+""" Unit test State """
 import unittest
 import models
 import os
-from models.city import City
-class TestCity(unittest.TestCase):
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """ Test for class State"""
+
     def test_docstring(self):
+        '''test if funcions, methods, classes
+        and modules have docstring'''
         msj = "Módulo does not has docstring"
-        self.assertIsNotNone(models.city.__doc__, msj)
+        self.assertIsNotNone(models.state.__doc__, msj)  # Modules
         msj = "Clase does not has docstring"
-        self.assertIsNotNone(City.__doc__, msj)
+        self.assertIsNotNone(State.__doc__, msj)  # Classes
+
     def test_executable_file(self):
-        is_read_true = os.access('models/city.py', os.R_OK)
+        '''test if file has permissions u+x to execute'''
+        # Check for read access
+        is_read_true = os.access('models/state.py', os.R_OK)
         self.assertTrue(is_read_true)
-        is_write_true = os.access('models/city.py', os.W_OK)
+        # Check for write access
+        is_write_true = os.access('models/state.py', os.W_OK)
         self.assertTrue(is_write_true)
-        is_exec_true = os.access('models/city.py', os.X_OK)
+        # Check for execution access
+        is_exec_true = os.access('models/state.py', os.X_OK)
         self.assertTrue(is_exec_true)
-    def test_init_city(self):
-        my_object = City()
-        self.assertIsInstance(my_object, City)
+
+    def test_init_State(self):
+        """test if an object is an type State"""
+        my_object = State()
+        self.assertIsInstance(my_object, State)
+
     def test_id(self):
-        my_objectId = City()
-        my_objectId1 = City()
+        """ test that id is unique """
+        my_objectId = State()
+        my_objectId1 = State()
         self.assertNotEqual(my_objectId.id, my_objectId1.id)
+
     def test_str(self):
-        my_strobject = City()
+        '''check if the output of str is in the specified format'''
+        my_strobject = State()
         _dict = my_strobject.__dict__
-        string1 = "[City] ({}) {}".format(my_strobject.id, _dict)
+        string1 = "[State] ({}) {}".format(my_strobject.id, _dict)
         string2 = str(my_strobject)
         self.assertEqual(string1, string2)
+
     def test_save(self):
-        my_objectupd = City()
+        """ check if date update when save """
+        my_objectupd = State()
         first_updated = my_objectupd.updated_at
         my_objectupd.save()
         second_updated = my_objectupd.updated_at
         self.assertNotEqual(first_updated, second_updated)
+
     def test_to_dict(self):
-        my_model3 = City()
+        '''check if to_dict returns a dictionary, if add a class
+        key with class name of the object and if updated_at and
+        created_at are converted to string object in ISO format.'''
+        my_model3 = State()
         my_dict_model3 = my_model3.to_dict()
         self.assertIsInstance(my_dict_model3, dict)
         for key, value in my_dict_model3.items():
             flag = 0
-            if my_dict_model3['__class__'] == 'City':
+            if my_dict_model3['__class__'] == 'State':
                 flag += 1
             self.assertTrue(flag == 1)
         for key, value in my_dict_model3.items():
@@ -48,5 +73,7 @@ class TestCity(unittest.TestCase):
                 self.assertIsInstance(value, str)
             if key == 'updated_at':
                 self.assertIsInstance(value, str)
+
+
 if __name__ == '__main__':
     unittest.main()
